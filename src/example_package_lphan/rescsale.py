e0a75558611c0a6d8503a01db40cd30ea819f4cc
leigh <leighphan@ucla.edu>
2025-07-17 07:20:06 -0700

    adding toml and src files

---
+++ src/example_package_lphan/rescsale.py
@@ -0,0 +1,14 @@
+import numpy as np 
+def rescale(input_array): 
+    """Rescales an array from 0 to 1.""" 
+    low = np.min(input_array) 
+    high = np.max(input_array) 
+    output_array = (input_array - low) / (high - low) 
+    return output_array
+
+def rescale_times10(input_array): 
+    """Rescales an array from 0 to 1.""" 
+    low = np.min(input_array) 
+    high = np.max(input_array) 
+    output_array = (input_array - low) / (high - low) 
+    return output_array * 10
